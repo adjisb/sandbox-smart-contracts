c4784b4379fd500b921e59a939025319446cd0d3
Ayush Tiwari <ayush@sandbox.game>
2023-04-24 18:57:37 +0530

    refactor: updated event name in Operator Filter

--- src/solc_0.8/OperatorFilterer/contracts/upgradeable/OperatorFiltererUpgradeable.sol
+++ src/solc_0.8/OperatorFilterer/contracts/upgradeable/OperatorFiltererUpgradeable.sol
@@ -9,7 +9,7 @@ import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Ini
 abstract contract OperatorFiltererUpgradeable is Initializable {
     IOperatorFilterRegistry public operatorFilterRegistry;
 
-    event LandRegistered(address indexed subscriptionOrRegistrant, bool subscribe);
+    event ContractRegistered(address indexed subscriptionOrRegistrant, bool subscribe);
 
     function __OperatorFilterer_init(address subscriptionOrRegistrantToCopy, bool subscribe) internal onlyInitializing {
         _register(subscriptionOrRegistrantToCopy, subscribe);
@@ -32,7 +32,7 @@ abstract contract OperatorFiltererUpgradeable is Initializable {
                 }
             }
         }
-        emit LandRegistered(subscriptionOrRegistrantToCopy, subscribe);
+        emit ContractRegistered(subscriptionOrRegistrantToCopy, subscribe);
     }
 
     modifier onlyAllowedOperator(address from) virtual {
